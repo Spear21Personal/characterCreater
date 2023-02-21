fcb8c2c7e5d0f4a5d06ab7eb77ba1c78f10d4b3e
Lloyd Spear <llspear@charter.net>
2023-02-21 14:10:14 -0500

    remove unused components (sample) add basic markup for character sheet

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { CharcterGenComponent } from './components/charcter-gen/charcter-gen.component';
 import { SvgAnimaitionComponent } from './components/svg-animaition/svg-animaition.component';
+import { CharacterSheetComponent } from './components/character-sheet/character-sheet.component';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import { SvgAnimaitionComponent } from './components/svg-animaition/svg-animaiti
     AbilityTileComponent,
     DropDownFormComponent,
     CharcterGenComponent,
-    SvgAnimaitionComponent
+    SvgAnimaitionComponent,
+    CharacterSheetComponent
   ],
 
   providers: [],
